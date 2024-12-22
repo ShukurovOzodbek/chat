98a4fe0eec5d87e794a929169ac0afff46716721
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-12-22 17:12:31 +0500

    created auth page and it's basic functions

---
+++ frontend/src/components/MailAuth.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-10/12 sm:w-7/12 md:w-3/12 flex flex-col items-center gap-6">
+        <h1 class="text-4xl">Chat App</h1>
+        <form class="w-full" @submit.prevent="login">
+            <input type="email"
+                class="py-3 px-4 w-full bg-gray-100 outline-gray-200 rounded-t-lg text-sm disabled:opacity-50"
+                placeholder="example@gmail.com" v-model="user.email">
+            <button type="submit" class="w-full bg-[#62A1DD] opacity-100 transition-all rounded-b-lg text-white p-2 text-lg disabled:opacity-40"
+                :disabled="isDisabled">Next</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const isDisabled = ref(true);
+const user = ref({ email: '' });
+
+function login() {
+
+}
+
+</script>
---
+++ frontend/src/pages/auth.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="h-screen">
+        <div class="h-full flex items-center justify-center">
+            <mail-auth v-if="isPhoneAuth" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const isPhoneAuth = ref(true);
+
+</script>
