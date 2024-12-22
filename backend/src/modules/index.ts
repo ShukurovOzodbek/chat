b2f70bc79f5738591066e2eeff633bebd75ee506
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2024-12-22 17:22:36 +0500

    auth router added to global router

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UsersController from './users/users.controller';
+import AuthController from './auth/auth.controller';
 
 const router = Router();
 
 router.use('/users', UsersController);
+router.use('/auth', AuthController);
 
 export default router;
